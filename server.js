d3cad59b20b202df678df6a4cb1105046b85e5a8
KaranberrySauce <karan.sidhu98@gmail.com>
2025-04-14 13:04:09 -0700

    Set header in the response to receive plain text data

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("request made");
+  console.log(req.url, req.method);
+
+  //set header content type
+  res.setHeader("Content-Type", "text/plain");
+
+  res.write()
 });
 
 server.listen(3000, "localhost", () => {
-  console.log("listening for requests on port: 3000"); 
+  console.log("listening for requests on port: 3000");
 });
