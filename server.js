1966df0fa09963174330e98fa653a0753ae5b858
KaranberrySauce <karan.sidhu98@gmail.com>
2025-04-17 11:58:32 -0700

    Install express

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ const _ = require("lodash");
 
 const server = http.createServer((req, res) => {
   //lodash
-  const num = _.random(0, 20);
-  console.log(num);
+  // const num = _.random(0, 20);
+  // console.log(num);
 
-  const greet = _.once(() => {
-    console.log("hello");
-  });
+  // const greet = _.once(() => {
+  //   console.log("hello");
+  // });
 
-  greet();
-  greet();
-  greet();
-  greet();
+  // greet();
+  // greet();
+  // greet();
+  // greet();
 
   //set header content type
   res.setHeader("Content-Type", "text/html");
