b963133a2a74ae755b2fe6a9742463f8912e6d32
KaranberrySauce <karan.sidhu98@gmail.com>
2025-04-17 17:44:07 -0700

    Create titles for each page

--- app.js
+++ app.js
@@ -10,23 +10,20 @@ app.set("view engine", "ejs");
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  //res.send("<p>Home page</p>");
-  res.render("index");
+  res.render("index", { title: "Home" });
 });
 
 app.get("/about", (req, res) => {
-  //res.send("<p>About page</p>");
-  res.render("about");
+  res.render("about", { title: "About" });
 });
 
-//redirects
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create New Blog" });
 });
 
 //404 page
 app.use((req, res) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { title: "404" });
 });
 
 /* This method will go down the list of requests until no page
