8e13baca54d0f199b55fab89208a197be29d1812
KaranberrySauce <karan.sidhu98@gmail.com>
2025-04-17 17:32:19 -0700

    Create 3 .ejs files and direct them

--- app.js
+++ app.js
@@ -6,18 +6,17 @@ const app = express();
 //register view engine
 app.set("view engine", "ejs");
 
-
 //listen for requests
 app.listen(3000);
 
 app.get("/", (req, res) => {
   //res.send("<p>Home page</p>");
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
   //res.send("<p>About page</p>");
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
 });
 
 //redirects
@@ -27,7 +26,7 @@ app.get("/about-us", (req, res) => {
 
 //404 page
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
 
 /* This method will go down the list of requests until no page
