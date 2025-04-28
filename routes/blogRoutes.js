5b6d93246df48b974cc7953ecbf982ff0f5ecf86
KaranberrySauce <karan.sidhu98@gmail.com>
2025-04-28 12:08:08 -0700

    Create controller for delete and details

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -4,32 +4,12 @@ const router = express.Router();
 
 router.get("/", blogController.blog_index);
 
-router.post("/", (req, res) => {
-  const blog = new Blog(req.body);
-
-  blog
-    .save()
-    .then((result) => {
-      res.redirect("/blogs");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.post("/", blogController.blog_create_post);
 
 router.get("/create", blogController.blog_create_get);
 
 router.get("/:id", blogController.blog_details);
 
-router.delete("/:id", (req, res) => {
-  const id = req.params.id;
-  Blog.findByIdAndDelete(id)
-    .then((result) => {
-      res.json({ redirect: "/blogs" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.delete("/:id", blogController.blog_delete);
 
 module.exports = router;
